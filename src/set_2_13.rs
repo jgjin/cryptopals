d70a75116b0ce40088c08e76c92a6096e64780cc
banana <jgjin@umich.edu>
2019-08-31 17:33:29 -0400

    clean

--- src/set_2_13.rs
+++ src/set_2_13.rs
@@ -1,5 +1,3 @@
-extern crate percent_encoding;
-
 use std::{
     collections::{
         BTreeMap,
@@ -13,12 +11,6 @@ use openssl::{
         encrypt,
     },
 };
-// use percent_encoding::{
-//     AsciiSet,
-//     CONTROLS,
-//     percent_decode,
-//     percent_encode,
-// };
 
 use crate::{
     set_2_9::{
@@ -110,116 +102,6 @@ impl ProfileCrypter {
         )
     }
 }
-
-// // Assume we know format, so function not needed
-// fn calc_bytes_before(
-//     encrypter: &dyn Encrypter,
-// ) -> usize {
-//     let repeated_input = "YELLOW_SUBMARINEYELLOW_SUBMARINE";
-//     let mut prefix = String::new();
-
-//     let block_size = detect_block_size(encrypter);
-//     while !repeated_chunks(
-//         &encrypter.encrypt(
-//             &format!("{}{}", prefix, repeated_input).into_bytes(),
-//         ).encrypted,
-//         block_size,
-//     ) {
-//         prefix.push('P');
-//     }
-
-//     let mut duplicate_index = 0;
-//     let mut chunks = BTreeSet::new();
-//     encrypter.encrypt(
-//         &format!("{}{}", prefix, repeated_input).into_bytes(),
-//     ).encrypted.chunks(block_size).take_while(|chunk| {
-//         if chunks.contains(chunk) {
-//             return false;
-//         }
-        
-//         chunks.insert(chunk.clone());
-//         true
-//     }).map(|_| {
-//         duplicate_index += 1;
-//     }).last();
-
-//     if prefix.is_empty() {
-//         return (duplicate_index - 1) * block_size
-//     }
-    
-//     (duplicate_index - 2) * block_size + block_size - prefix.len()
-// }
-
-// // Assume we know format, so function not needed
-// // Function does not work when & and = are removed or replaced with percent encoded equivalents
-// // because when trying to guess character after & or =, cannot give & or = in user input
-// // without that & or = being removed or replaced
-// fn guess_nth_byte_after(
-//     nth: usize,
-//     known_so_far: &Vec<u8>,
-//     encrypter: &dyn Encrypter,
-//     prefix: &Vec<u8>,
-//     chunk_offset: usize,
-// ) -> Option<u8> {
-//     let block_size = detect_block_size(encrypter);
-//     let mut input = vec!['I' as u8; block_size - (nth % block_size) - 1];
-//     let chunk_to_cmp = nth / block_size + chunk_offset;
-
-//     let mut true_input = vec![];
-//     true_input.append(&mut prefix.clone());
-//     true_input.append(&mut input);
-
-//     let mut comparable = encrypter.encrypt(&true_input).encrypted;
-//     if (chunk_to_cmp + 1) * block_size > comparable.len() {
-//         return None;
-//     }
-//     comparable = comparable.chunks(block_size)
-//         .skip(chunk_to_cmp).next()
-//         .expect("error in code").to_vec();
-
-//     true_input.append(&mut known_so_far.clone());
-//     let mut guess = 0;
-//     true_input.push(guess);
-//     while encrypter.encrypt(&true_input).encrypted.chunks(block_size)
-//         .skip(chunk_to_cmp).next()
-//         .expect("error in code").to_vec() != comparable {
-//             if guess == 255u8 {
-//                 // because & and = are eaten by encoder
-//                 println!("no match among all 256 byte values");
-//                 return None;
-//             }
-
-//             guess += 1;
-//             *true_input.last_mut().expect("error in code") = guess;
-//     }
-
-//     Some(guess)
-// }
-
-// fn get_bytes_after(
-//     encrypter: &dyn Encrypter,
-// ) -> Vec<u8> {
-//     let bytes_before = calc_bytes_before(encrypter);
-//     let block_size = detect_block_size(encrypter);
-//     let prefix = vec!['P' as u8; block_size - (bytes_before % block_size)];
-//     let chunk_offset = (bytes_before + prefix.len()) / block_size;
-
-//     let mut nth = 0;
-//     let mut known_so_far = vec![];
-    
-//     while let Some(byte) = guess_nth_byte_after(
-//         nth,
-//         &known_so_far,
-//         encrypter,
-//         &prefix,
-//         chunk_offset,
-//     ) {
-//         nth += 1;
-//         known_so_far.push(byte);
-//     }
-
-//     known_so_far
-// }
 
 pub fn main(
 ) {
